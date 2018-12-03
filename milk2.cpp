266308bd814e2bdd41fd3c6e08b380c658467b4e
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-12-03 09:49:59 -0500

    Update milk2.cpp

--- milk2.cpp
+++ milk2.cpp
@@ -4,46 +4,53 @@ PROG: milk2
 LANG: C++
 */
 
+
 #include <fstream>
 #include <bitset>
 #include <iostream>
 
 int main(){
-  
-  std::ifstream input("milk2.in");
-  int n;
-  input >> n;
-  int len = 0;
-  int firststart;
-  std::bitset<1000000> times;
-  for (int i = 0; i < n; i++){
-    int start; int end;
-    firststart = start;
-    input >> start >> end;
-    
-    for (int j = start; j < end; j++)
-      times[j] = true;
-    
-    if (end > len) len = end;
-    if (start < firststart) firststart = start;
-  }
-  input.close();
-  
-  int longestMilk = 0;
-  int longestNoMilk = 0;
-  int length;
-  bool curr;
-  for (int i = firststart; i < len; i += length){
-    curr = times[i];
-    length = 0;
-    for (int j = i; j < len; j++)
-      if (times[j] == curr) length++;
-    if (curr) if (longestMilk < length) longestMilk = length;
-    else if (longestNoMilk < length) longestNoMilk = length;
-  }
-  
-  std::ofstream output("milk2.out");
-  output << longestMilk << " " << longestNoMilk << "\n";
-  output.close();
-  return 0;
+	
+	std::ifstream input("milk2.in");
+	int n;
+	input >> n;
+	int len = 0;
+	int firststart = 1000000;
+	std::bitset<1000000> times;
+	for (int i = 0; i < n; i++){
+		int start; int end;
+		input >> start >> end;
+		
+		for (int j = start; j < end; j++)
+			times[j] = true;
+		
+		if (end > len) len = end;
+		if (start < firststart) firststart = start;
+	}
+	input.close();
+	
+	int longestMilk = 0;
+	int longestNoMilk = 0;
+	int length;
+	bool curr;
+	curr = times[firststart];
+	//std::cout << firststart << "\n" << curr;
+	for (int i = firststart; i < len; i++){
+		if (times[i] == curr) {
+			length++;
+			//std::cout << curr;
+		}
+		else{
+			if (curr) if (longestMilk < length) longestMilk = length;
+			if (!curr) if (longestNoMilk < length) longestNoMilk = length;
+			length = 1;
+			curr = times[i];
+			//std::cout << curr;
+		}
+	}
+	
+	std::ofstream output("milk2.out");
+	output << longestMilk << " " << longestNoMilk << "\n";
+	output.close();
+	return 0;
 }
