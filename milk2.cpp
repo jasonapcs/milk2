5f776c12b8b6f754239841d77c2d6e461853857d
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-12-03 09:05:19 -0500

    Update milk2.cpp

--- milk2.cpp
+++ milk2.cpp
@@ -7,3 +7,40 @@ LANG: C++
 #include <fstream>
 #include <bitset>
 #include <iostream>
+
+int main(){
+  
+  std::ifsteam input("milk2.in");
+  int n;
+  input >> n;
+  int len = 0;
+  std::bitset<1000000> times;
+  for (int i = 0; i < n; i++){
+    int start; int end;
+    input >> start >> end;
+    
+    for (int j = start; j < end; j++)
+      times[j] = true;
+    
+    if (end > len) len = end;
+  }
+  input.close();
+  
+  int longestMilk = 0;
+  int longestNoMilk = 0;
+  int len;
+  bool curr;
+  for (int i = 0; i < len; i += len){
+    curr = times[i];
+    len = 0;
+    for (int j = i + 1; j < len; j++)
+      if (times[j] == curr) len++;
+    if (curr) if (longestMilk < len) longestMilk = len;
+    else if (longestNoMilk < len) longestNoMilk = len;
+  }
+  
+  std::ofstream output("milk2.out");
+  output << longestMilk << " " << longestNoMilk << "\n";
+  output.close();
+  return 0;
+}
